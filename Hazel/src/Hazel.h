299825e947573eae10aa488d2eaed5636549f0bd
Alex Ananev <arrekusu.a@gmail.com>
2020-06-14 07:24:30 +0100

    Initial Commit

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//For Client Use Only
+
+#include "Hazel/Application.h"
+
+// ----------- Entry Point ---------------
+#include "Hazel/EntryPoint.h"
+//----------------------------------------
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Hazel {
+	class HAZEL_API Application
+	{
+	public:
+		void Run();
+	};
+
+	//TBD in a client
+	Application* CreateApplication();
+}
+
