73b629addf491765cfd30ace2887be37c56630a5
Alex Ananev <arrekusu.a@gmail.com>
2020-06-15 08:49:14 +0100

    EventSystem Initial Commit

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,9 +1,14 @@
 #include "Application.h"
 
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Log.h"
 
 namespace Hazel {
 	void Application::Run()
 	{
+		KeyPressedEvent e{ 15, 5 };
+		HZ_TRACE(e);
+
 		while (true);
 	}
 }
