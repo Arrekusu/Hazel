ed10b56b1260da0b5a8b4274f6d86a6aee5c9ae0
Alex Ananev <arrekusu.a@gmail.com>
2020-06-18 10:58:43 +0100

    GLFW Window added

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "Core.h"
+#include "Window.h"
 
 namespace Hazel {
 	class HAZEL_API Application
 	{
 	public:
+		Application();
+		~Application();
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	//TBD in a client
